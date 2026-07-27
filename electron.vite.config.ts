502857fc2428e4e79db0f44cfd8112182ba816d3
Leep <1197735110@qq.com>
2026-07-27 22:54:34 +0800

    init

---
+++ electron.vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  main: {},
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        '@': resolve('src/renderer/src')
+      }
+    },
+    plugins: [
+      vue(), 
+      tailwindcss()
+    ]
+  }
+})
